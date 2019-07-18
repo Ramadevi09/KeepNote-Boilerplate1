585ab1e7fcce7352389fc0c505bb6e86472a15ec
Ramadevi09 <rdevi2850@gmail.com>
2019-07-18 16:34:51 +0530

    annotations

---
+++ src/main/java/com/stackroute/keepnote/dispatcherServlet/DispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.stackroute.keepnote.dispatcherServlet;
+
+import com.stackroute.keepnote.configuratuionfiles.WebApplicationContextConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class DispatcherServletInitializer implements WebApplicationInitializer {
+
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+
+        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        webCtx.register(WebApplicationContextConfig.class);
+        webCtx.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(webCtx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+}
